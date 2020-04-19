9152d15476a23c49f5bfec9ba50c3e8359e2829a
Jakub Cichacki <kuba.cichacki@o2.pl>
2020-04-19 12:43:48 +0200

    Room counter which detect upper right corner of the room

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj numer szuaknego elementu: ");
-        String numberFromKeyboard = scanner.nextLine();
-        int number = Integer.parseInt(numberFromKeyboard);
-
-        System.out.println("Fib (" + number + ") = " + fibIteration(number));
-        System.out.println("Fib (" + number + ") = " + fibRecursion(number));
-
+        int [][] planOfHouse = new int[][] {
+                {1,1,1,1,1,1,1,1,1,1},
+                {1,0,1,1,0,0,1,0,0,1},
+                {1,0,1,0,0,0,1,1,0,1},
+                {1,1,1,0,0,0,1,0,0,1},
+                {1,1,1,1,1,1,1,1,1,1}
+        };
+        int result = counterOfRooms(planOfHouse);
+        System.out.println(result);
     }
 
-    public static long fibIteration(int number) {
-        long fib0 = 0;
-        long fib1 = 1;
-
-        if (number == 0)
-            return fib0;
-
-        else {
-            for (int i = 2; i <= number; i++) {
-                fib1 += fib0;
-                fib0 = fib1 - fib0;
+    public static int counterOfRooms(int [][] planOfHouse) {
+        int counter = 0;
+        for (int i = 1; i < planOfHouse.length; i++) {
+            for (int j = 1; j < planOfHouse[i].length; j++) {
+                if (planOfHouse[i][j] == 0 && planOfHouse[i-1][j] != 0 && planOfHouse[i][j-1] != 0)
+                    counter++;
             }
-            return fib1;
         }
-    }
-
-    public static long fibRecursion (int number) {
-        if (number == 0)
-            return 0;
-
-        else if(number == 1)
-            return 1;
-
-         else return fibRecursion(number-2)+fibRecursion(number-1);
+        return counter;
     }
 }
